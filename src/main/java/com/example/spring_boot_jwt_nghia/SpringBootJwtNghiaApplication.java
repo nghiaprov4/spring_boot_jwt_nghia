f29b184a3ee9c39327adc16d5d9e793095a09cbe
DESKTOP-41VA80C\IBM <thieuquangnghia2000@gmail.com>
2021-09-23 16:24:01 +0700

    Initial commit

---
+++ src/main/java/com/example/spring_boot_jwt_nghia/SpringBootJwtNghiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_nghia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtNghiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtNghiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_boot_jwt_nghia/SpringBootJwtNghiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_nghia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtNghiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
